f9006b3a8b8e16d94f91a9778759a9677ba6550a
focaG <60098824+Edamatec@users.noreply.github.com>
2024-04-08 11:15:54 -0300

    Feat: Criando lógica para validar um produto existente

--- Curso Outbox Saga Orquestrado/product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/OrderProducts.java
+++ Curso Outbox Saga Orquestrado/product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/OrderProducts.java
@@ -5,11 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class OrderProducts {
 
     private Product product;
     private int quantity;
-
 }
